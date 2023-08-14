fa66244916405c1c200c780fb127e20600ddec3d
Eshan Goel <67696491+Eshan27@users.noreply.github.com>
2023-08-14 01:15:47 -0400

    Changes

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,36 +1,38 @@
-import './App.css';
 import React from 'react';
-import Navbar from './component/layout/Navbar';
-import User from './component/users/User';
-import Search from './component/users/Search';
-import axios from 'axios';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import User from './components/users/User';
+import Alert from './components/layout/Alert';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
-class App extends React.Component{
-  state={
-    users:[],
-    loading: false
-  }
-  // async componentDidMount(){
-  //   this.setState({loading: true});
-  //   const res= await axios.get('https://api.github.com/users');
-  //   this.setState({users: res.data, loading: false});
-  // }
-  searchUsers=async text=>{
-    const res= await axios.get('https://api.github.com/search/users?q=${text}');
-    this.setState({users: res.data.items, loading: false});
-  }
-  render(){
-    return (
-      <div className="App">
-        <Navbar/>
-        <div className='container'>
-          < Search searchUsers={this.searchUsers}/>
-        <User loading={this.state.loading} users={this.state.users}/>
-        </div>
-      </div>
-    );
-  }
-  
-}
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <Navbar />
+            <div className='container'>
+              <Alert />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
+  );
+};
 
 export default App;
